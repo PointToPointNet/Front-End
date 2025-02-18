8aff478f041427ca8ed9efbd6957d11e1abcb88f
윤혜경 <yunhyegyeong@yunhyegyeong-ui-MacBookAir.local>
2025-02-18 10:28:18 +0900

    file rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import './styles/dash.scss';
 
 function App() {
 
   return (
     <>
+      <h1>sample</h1>
     </>
   )
 }
