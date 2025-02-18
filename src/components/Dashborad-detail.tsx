e05ff506bc0847756d769acdefcc296e33f386bd
윤혜경 <yunhyegyeong@yunhyegyeong-ui-MacBookAir.local>
2025-02-18 10:24:35 +0900

    file setting

---
+++ src/components/Dashborad-detail.tsx
@@ -0,0 +1,3 @@
+// 각 데이터들을 나타내는 컴포넌트들이이 모여있는 상세페이지
+// 구성 :
+// Nav-bar.tsx, Board-packet.tsx, Board-ports.tsx, Board-runtime.tsx, Board-status.tsx, Board-users.tsx
---
+++ src/components/Nav-bar.tsx
@@ -0,0 +1,3 @@
+// 왼쪽 사이드 이동 버튼
+// 구성 :
+// Nav-bar-add.tsx, Nav-bar-button.tsx
