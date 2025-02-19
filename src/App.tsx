6864e545e6c90ff60e6fd1e66a94078a99749283
윤혜경 <yunhyegyeong@yunhyegyeong-ui-MacBookAir.local>
2025-02-19 18:12:48 +0900

    setting...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import DashboardDetail from './components/Dashboard-detail.tsx';
 import detail from './styles/dashboard-detail.module.scss';
 
 const App: React.FC = () => {
   
   return <div className={detail.container}>
-    <Dashboard-detail></Dashboard-detail>
+    <DashboardDetail></DashboardDetail>
   </div>
 
 }
--- src/components/Dashboard-detail.tsx
+++ src/components/Dashboard-detail.tsx
@@ -1,21 +1,35 @@
 // 각 데이터들을 나타내는 컴포넌트들이이 모여있는 상세페이지
 // 구성 :
 // Nav-bar.tsx, Board-packet.tsx, Board-ports.tsx, Board-runtime.tsx, Board-status.tsx, Board-users.tsx
-import './Nav-var.tsx'
-import './Nav-bar-add.tsx';
-import './Nav-bar-button.tsx';
+import NavBar from './Nav-bar.tsx'
+import BoardRuntime from './Board-runtime.tsx';
+import BoardStatus from './Board-status.tsx';
+import BoardResspeed from './Board-resspeed.tsx';
+
 import detail from './styles/dashboard-detail.module.scss';
 
-const NavBar: React.FC = () => {
+const DashboardDetail: React.FC = () => {
 
-  return <div className={detail.navbar}>
-    <NavBar></NavBar>
-    <div className={detail.board}>
-        <Board-rumtime></Board-rumtime>
-        
-    </div>
-  </div>
+  return <>
+        <div className={detail.navbar}>
+        <NavBar></NavBar>
+        </div>
+        <div className={detail.board}>
+            <div className={detail.boardTop}>
+                <BoardRuntime></BoardRuntime>
+                <BoardStatus></BoardStatus>
+                <BoardResspeed></BoardResspeed>
+            </div>
+            <div className={detail.boardBody}>
+                <BoardPorts></BoardPorts>
+                <BoardPacket></BoardPacket>
+                <BoardUsers></BoardUsers>
+            </div>
+        </div>
+    </>
+    
+  
 
 }
 
-export default NavBar;
+export default DashboardDetail;
